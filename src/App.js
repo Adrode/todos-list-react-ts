b84ff3721c70e81adf7faa58c8d68aa3cd0f9ca6
Adrian Wozniak <adrianwozniak20@gmail.com>
2023-08-31 13:23:22 +0200

    added tasks array and deleted 'js-' class names

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+const tasks = [
+  {id: 1, content: "learn React", done: false},
+  {id: 2, content: "eat dinner", done: true},
+];
+const hideDoneTasks = false;
+
 function App() {
   return (
     <div className="App">
@@ -9,17 +15,17 @@ function App() {
         <header className="section__header">
           <h2 className="section__title">Add new task</h2>
         </header>
-        <form className="form js-form">
-          <input className="form__textInput js-newTask" placeholder="What is there to do?" />
-          <button className="form__submitButton js-addNewTask">Add task</button>
+        <form className="form">
+          <input className="form__textInput" placeholder="What is there to do?" />
+          <button className="form__submitButton">Add task</button>
         </form>
       </section>
       <section className="section">
         <header className="section__header">
           <h2 className="section__title">Tasks list</h2>
-          <div className="buttons__container js-featureButtons"></div>
+          <div className="buttons__container"></div>
         </header>
-        <ul className="list__tasksList js-tasks">
+        <ul className="list__tasksList">
         </ul>
       </section>
     </div>
