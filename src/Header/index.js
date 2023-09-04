6a9be4fb4b87dd04462d8338edcdc9a34eb559cf
Adrian Wozniak <adrianwozniak20@gmail.com>
2023-09-04 19:24:46 +0200

    removed unnecessary className from header component

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,8 +1,6 @@
-import "./style.css";
-
 const Header = ({title}) => (
-    <header className="header">
-        <h1 className="header__title">{title}</h1>
+    <header>
+        <h1>{title}</h1>
     </header>
 );
 
