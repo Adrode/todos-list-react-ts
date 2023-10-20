53a0ffe29e16c8a1404d65687f14f78f422a2ab2
Adrian Wozniak <adrianwozniak20@gmail.com>
2023-10-20 12:08:22 +0200

    deleted 'utils' catalog (added previously for tests)

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import Buttons from "./Buttons";
 import Section from "./Section";
 import Header from "./Header";
 import Container from "./Container";
-import * as hello from "./utils/hello";
-
-hello.hello();
-console.log(hello.name);
 
 const tasks = [
   { id: 1, content: "learn React", done: false },
