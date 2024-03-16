a80f9f6464595c82414f081996bfe99ad73a5e75
Adrian Wozniak <adrianwozniak20@gmail.com>
2024-03-16 17:19:46 +0100

    about Author information added

--- src/features/AuthorPage/index.js
+++ src/features/AuthorPage/index.js
@@ -7,7 +7,16 @@ const AuthorPage = () => (
         <Section
             title="Adrian Woźniak"
             body={
-                <>kekw</>
+                <>
+                    Nazywam się <strong>Adrian</strong>. Mieszkam we Wrocławiu. Obecnie pracuję jako
+                    kierownik restauracji w jednej z wrocławskich restauracji KFC. Ukończyłem technikum informatyczne i po czasie
+                    uznałem że chciałbym wrócić do zawodu, także zdecydowałem się na podjęcie kursu u <b>Krzyśka i ekipy w YouCode.</b>
+                    <br /><br />Interesuję się głównie muzyką oraz motocyklami.<br />Jeżeli chodzi o muzykę, to
+                    słucham wielu rzeczy z różnych
+                    gatunków, tj. <i>rock, rap, blues, funk, pop, punk, grunge, metal, techno,</i> itp.<br />
+                    Jeszcze rok temu posiadałem motocykl, Suzuki SV650, lecz sprzedałem go ponieważ potrzebowałem samochodu,
+                    lecz obecnie oszczędzam aby kupić kolejny motocykl, ponieważ <em>bardzo</em> mi tego brakuje.
+                </>
             }
         />
     </>
