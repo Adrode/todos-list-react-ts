6a4d272186c14e6771918d212d9d74c69a442ee8
Adrian Woźniak <adrianwozniak20@gmail.com>
2025-02-13 22:10:20 +0100

    tasksSlice adjusted to TS

--- src/features/tasks/tasksSaga.ts
+++ src/features/tasks/tasksSaga.ts
@@ -1,12 +1,15 @@
+import { Task } from "./types";
 import { call, delay, put, takeLatest, select, takeEvery } from "redux-saga/effects";
 import { fetchExampleTasks, selectTasks, fetchExampleTasksSuccess, fetchExampleTasksError } from "./tasksSlice";
 import { getExampleTasks } from "./getExampleTasks";
 import { saveTasksInLocalStorage } from "./tasksLocalStorage";
 
+type TaskProps = { tasks: Task[] };
+
 function* fetchExampleTasksHandler() {
     try {
         yield delay(1000);
-        const exampleTasks = yield call(getExampleTasks);
+        const exampleTasks: TaskProps = yield call(getExampleTasks);
         yield put(fetchExampleTasksSuccess(exampleTasks));
     } catch (error) {
         yield put(fetchExampleTasksError());
@@ -15,7 +18,7 @@ function* fetchExampleTasksHandler() {
 };
 
 function* saveTasksInLocalStorageHandler() {
-    const tasks = yield select(selectTasks);
+    const tasks: TaskProps = yield select(selectTasks);
     yield call(saveTasksInLocalStorage, tasks);
 }
 
---
+++ src/features/tasks/tasksSlice.ts
@@ -0,0 +1,90 @@
+import { Task } from "./types";
+import { RootState } from "../../store";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { getTasksFromLocalStorage } from "./tasksLocalStorage";
+
+interface InitialStateProps {
+    tasks: Task[];
+    hideDone: boolean;
+    loading: boolean;
+};
+
+const initialState: InitialStateProps = {
+    tasks: getTasksFromLocalStorage().tasks,
+    hideDone: false,
+    loading: false,
+}
+
+const tasksSlice = createSlice({
+    name: 'tasks',
+    initialState,
+    reducers: {
+        addTask: ({ tasks }, { payload: task }: PayloadAction<Task>) => {
+            tasks.push(task);
+        },
+        removeTask: ({ tasks }, { payload: id }: PayloadAction<number>) => {
+            let index = tasks.findIndex(task => task.id === id);
+            tasks.splice(index, 1);
+        },
+        toggleHideDone: ({ hideDone }) => {
+            hideDone = !hideDone;
+        },
+        toggleTaskDone: ({ tasks }, { payload: id }: PayloadAction<number>) => {
+            const index = tasks.findIndex(task => task.id === id);
+            tasks[index].done = !tasks[index].done;
+        },
+        toggleAllDone: ({ tasks }) => {
+            for (const task of tasks) {
+                task.done = true;
+            };
+        },
+        fetchExampleTasks: ({ loading }) => {
+            loading = true;
+        },
+        fetchExampleTasksSuccess: ({ tasks, loading }, { payload: tasksFetched }: PayloadAction<Task[]>) => {
+            tasks = tasksFetched;
+            loading = false;
+        },
+        fetchExampleTasksError: ({ loading }) => {
+            loading = false;
+        },
+    },
+});
+
+const selectTasksState = (state: RootState) => state.tasks;
+
+export const {
+    addTask,
+    removeTask,
+    toggleHideDone,
+    toggleTaskDone,
+    toggleAllDone,
+    fetchExampleTasks,
+    fetchExampleTasksSuccess,
+    fetchExampleTasksError,
+} = tasksSlice.actions;
+
+export const selectTasks = (state: RootState) => selectTasksState(state).tasks;
+export const selectHideDone = (state: RootState) => selectTasksState(state).hideDone;
+
+export const selectIsEveryTaskDone = (state: RootState) =>
+    selectTasks(state).every(({ done }) => done);
+
+export const selectIsTasksListEmpty = (state: RootState) =>
+    selectTasks(state).length === 0;
+
+export const selectTaskById = (state: RootState, taskId: number) =>
+    selectTasks(state).find(({ id }) => id === taskId);
+
+export const selectTasksByQuery = (state: RootState, query: string) => {
+    const tasks = selectTasks(state);
+    if (!query || query.trim() === "") {
+        return tasks;
+    };
+
+    return selectTasks(state).filter(({ content }) => content.toUpperCase().includes(query.trim().toUpperCase()));
+};
+
+export const selectLoadingStatus = (state: RootState) => selectTasksState(state).loading;
+
+export default tasksSlice.reducer;
