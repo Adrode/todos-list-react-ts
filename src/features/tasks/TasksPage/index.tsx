2360f7de57112ea8a6168fa90401812820c2a93d
Adrian Woźniak <adrianwozniak20@gmail.com>
2025-02-25 20:03:49 +0100

    TasksPage changed to work on TS

---
+++ src/features/tasks/TasksPage/index.tsx
@@ -0,0 +1,35 @@
+import { useSelector } from "react-redux";
+import { selectTasks } from "../tasksSlice";
+import { Container } from "../../../common/Container";
+import Form from "./Form";
+import TasksList from "./TasksList";
+import Buttons from "./Buttons";
+import Section from "../../../common/Section";
+import Header from "../../../common/Header";
+import Search from "./Search";
+import LoadingButton from "./LoadingButton";
+
+const TasksPage = () => (
+  <Container>
+    <Header title="Tasks list" />
+
+    <Section
+      title="Add new task"
+      extraHeaderContent={<LoadingButton />}
+      body={<Form />}
+    />
+
+    <Section
+      title="Search engine"
+      body={<Search />}
+    />
+
+    <Section
+      title="Tasks list"
+      extraHeaderContent={<Buttons />}
+      body={<TasksList />}
+    />
+  </Container>
+);
+
+export default TasksPage;
