34cd2d42994a4b15ddb8b0af35095f10ca18ca12
Adrian Woźniak <adrianwozniak20@gmail.com>
2025-02-26 19:38:50 +0100

    getExampleTask changed error throwing

--- src/features/tasks/getExampleTasks.ts
+++ src/features/tasks/getExampleTasks.ts
@@ -4,7 +4,7 @@ export const getExampleTasks = async () => {
     const response = await fetch("/todos-list-react-ts/exampleTasks.json");
 
     if (!response.ok) {
-        new Error(response.statusText);
+        throw new Error(response.statusText);
     }
 
     return await response.json() as Task[];
